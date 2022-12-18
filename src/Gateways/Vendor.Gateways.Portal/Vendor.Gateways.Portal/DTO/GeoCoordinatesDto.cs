4de0414771ba241c714800c3c943386efc5dd5b9
BatMitio <mitko_zlia@abv.bg>
2022-12-18 23:40:03 +0200

    Successfully creted geolocation functionality in test Blazor page to use when creating a vending machine

---
+++ src/Gateways/Vendor.Gateways.Portal/Vendor.Gateways.Portal/DTO/GeoCoordinatesDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Vendor.Gateways.Portal.DTO;
+
+public class GeoCoordinatesDto
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public double Accuracy { get; set; }
+}
