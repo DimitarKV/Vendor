d2e2191f45f793c1c42fc23991842e3633eb5dd8
BatMitio <mitko_zlia@abv.bg>
2023-05-03 23:29:07 +0300

    Added validations to form for product creation and fixed a bug

--- src/Gateways/Vendor.Gateways.Portal/Vendor.Gateways.Portal/Models/RegisterProductModel.cs
+++ src/Gateways/Vendor.Gateways.Portal/Vendor.Gateways.Portal/Models/RegisterProductModel.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Components.Forms;
 
 namespace Vendor.Gateways.Portal.Models;
 
 public class RegisterProductModel
 {
+    [Required]
     public string Name { get; set; }
+    [Required]
     public IBrowserFile Image { get; set; }
 }
